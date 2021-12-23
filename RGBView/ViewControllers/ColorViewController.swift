caf5fac026fd3a8d8f05cf9c3e184c88c545cb55
aldvs <andrewgogol777@gmail.com>
2021-12-23 22:22:39 +0300

    Update repositories and add a new StartViewController

---
+++ RGBView/ViewControllers/ColorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  RGBView
+//
+//  Created by admin on 10.12.2021.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    
+    
+    //MARK: - Life Cycles Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - IB Actions
+    
+    @IBAction func sliderAction() {
+
+    }
+}
+
